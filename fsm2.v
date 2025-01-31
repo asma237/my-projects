eeff993513b345d7626a6cc7b49075c0ae29f299
asma237 <167109553+asma237@users.noreply.github.com>
2025-01-31 22:24:31 +0530

    Add files via upload

---
+++ fsm2.v
@@ -0,0 +1,32 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next;
+
+    always @(*) begin
+        // State transition logic
+        case(state)
+            OFF: next=j?ON:OFF;
+            ON: next=k?OFF:ON;
+            default: next=OFF;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if (areset)
+            state<=OFF;
+        else
+            state<=next;
+    end
+
+    // Output logic
+    assign out = (state == ON);
+
+endmodule
+
---
+++ fsm3comb.v
@@ -0,0 +1,21 @@
+module top_module(
+    input in,
+    input [1:0] state,
+    output reg [1:0] next_state,
+    output out); //
+
+    parameter A=0, B=1, C=2, D=3;
+
+    // State transition logic: next_state = f(state, in)
+    always @(*) begin
+        case (state)
+            2'b00: next_state= in?B:A;
+            2'b01: next_state= in?B:C;
+            2'b10: next_state= in?D:A;
+            2'b11: next_state= in?B:C;
+            default: next_state= A;
+        endcase
+    end
+    // Output logic:  out = f(state) for a Moore state machine
+    assign out= (state==D);
+endmodule
